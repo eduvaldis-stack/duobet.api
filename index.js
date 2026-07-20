2634e36bfda995fe651de96320ba91c8c82b4a80
eduvaldis-stack <eduvaldis@gmail.com>
2026-07-20 11:49:32 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default function handler(_req, res) {
-  res.status(200).json({
-    ok: true,
-    service: "Agencia Duobet Football API",
-    fixturesEndpoint: "/api/fixtures?date=2026-07-20&timezone=America%2FGuayaquil"
-  });
-}
